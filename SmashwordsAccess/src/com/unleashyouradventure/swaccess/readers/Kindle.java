727dc07320d26c8919924ca894694bda5c6b20b9
UnleashYourAdventure@gmail.com <UnleashYourAdventure@gmail.com@eef7ba7c-9e30-9691-7414-a8fb7c7904cd>
2013-12-31 03:21:39 +0000

    Launch Kindle app directly

--- SmashwordsAccess/src/com/unleashyouradventure/swaccess/readers/Kindle.java
+++ SmashwordsAccess/src/com/unleashyouradventure/swaccess/readers/Kindle.java
@@ -1,5 +1,10 @@
 package com.unleashyouradventure.swaccess.readers;
 
+import java.io.File;
+
+import android.content.Context;
+import android.content.Intent;
+
 import com.unleashyouradventure.swapi.retriever.Book.FileType;
 
 public class Kindle extends ReaderWithFolder {
@@ -15,5 +20,14 @@ public class Kindle extends ReaderWithFolder {
 
     public String getReaderLink() {
         return "market://details?id=com.amazon.kindle";
+    }
+
+    @Override
+    public CopyToReaderResult addBookToReader(File file, Context context) {
+        CopyToReaderResult result = super.addBookToReader(file, context);
+        // Now launch Kindle
+        Intent LaunchIntent = context.getPackageManager().getLaunchIntentForPackage("com.amazon.kindle");
+        context.startActivity(LaunchIntent);
+        return result;
     }
 }
