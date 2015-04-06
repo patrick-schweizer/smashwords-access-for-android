a75f80aef9281fb2da26d16250fe56a1fe389c2c
Patrick <patrick.schweizer@goeuro.com>
2015-04-06 14:12:09 +0200

    Move to Android Studio, using latest version of sw api bugfix for price over 1000$ new version 16

--- app/src/main/java/com/unleashyouradventure/swaccess/util/Format.java
+++ app/src/main/java/com/unleashyouradventure/swaccess/util/Format.java
@@ -4,13 +4,13 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-import com.unleashyouradventure.swapi.retriever.json.JPrices;
+import com.unleashyouradventure.swapi.model.SwPrices;
 
 public class Format {
 
     private static DecimalFormat priceFormat = new DecimalFormat("0.00", DecimalFormatSymbols.getInstance(new Locale("en_US")));
 
-    public static String getPrice(JPrices prices) {
+    public static String getPrice(SwPrices prices) {
         double amount = 0.00;
         if (prices != null && prices.getPrices().size() > 0)
             amount = prices.getPrices().get(0).getAmount();
